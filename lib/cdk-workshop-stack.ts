d9ec96e9c30b7fa052188497fbaa22dc68e746f0
Charles Anesu Kombo <62628677+Charl070@users.noreply.github.com>
2024-05-22 19:26:06 +0200

    feat: added hit couter function and dynamo db instance

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,21 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
-import * as Lambda from 'aws-cdk-lib/aws-lambda'
-import * as awsgw from 'aws-cdk-lib/aws-apigateway'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-  
-  // defines an AWS Lambda resource
-  const hello = new Lambda.Function(this, 'HelloHandler', {
-    runtime: Lambda.Runtime.NODEJS_16_X, // execution environment
-    code: Lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
-    handler: 'hello.handler'  // file is "hello", function is "handler"
-  });
 
-  // defines an API Gateway REST API resource backed by our "hello" function.
-  new awsgw.LambdaRestApi(this, 'Endpoint',{
-    handler: hello
-  });
-}
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+  }
 }
